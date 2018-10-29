2e62cb2787e8e275a1c8b7fff8bdc54cd069befb
Nipun Haldar <nipunhaldar24@gmail.com>
2018-10-29 20:39:02 +0530

    Final Build v3

--- frontend/build/precache-manifest.716b743614baab81af55c79911d2e78a.js
+++ frontend/build/precache-manifest.716b743614baab81af55c79911d2e78a.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "2cded645684824f3a211",
-    "url": "/static/js/main.2cded645.chunk.js"
+    "revision": "577ebef820bd0f9f51c6",
+    "url": "/static/js/main.577ebef8.chunk.js"
   },
   {
     "revision": "658cc27c1edf0fa2818c",
     "url": "/static/js/1.658cc27c.chunk.js"
   },
   {
-    "revision": "2cded645684824f3a211",
+    "revision": "577ebef820bd0f9f51c6",
     "url": "/static/css/main.93028f67.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/css/1.253daef8.chunk.css"
   },
   {
-    "revision": "c19ef1020dab1e7e0045ee7718d9b130",
+    "revision": "28de8addc49435359073f3fc6e796537",
     "url": "/index.html"
   }
 ];
