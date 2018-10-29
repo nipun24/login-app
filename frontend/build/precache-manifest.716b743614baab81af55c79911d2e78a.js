e128e75d3db673d7aed2fdfbdd65575328eb3d80
Nipun Haldar <nipunhaldar24@gmail.com>
2018-10-29 19:34:13 +0530

    New frontend build

--- frontend/build/precache-manifest.716b743614baab81af55c79911d2e78a.js
+++ frontend/build/precache-manifest.716b743614baab81af55c79911d2e78a.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "01e3ed94fe60db2717c5",
-    "url": "/static/js/main.01e3ed94.chunk.js"
+    "revision": "577ebef820bd0f9f51c6",
+    "url": "/static/js/main.577ebef8.chunk.js"
   },
   {
     "revision": "658cc27c1edf0fa2818c",
     "url": "/static/js/1.658cc27c.chunk.js"
   },
   {
-    "revision": "01e3ed94fe60db2717c5",
+    "revision": "577ebef820bd0f9f51c6",
     "url": "/static/css/main.93028f67.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/css/1.253daef8.chunk.css"
   },
   {
-    "revision": "b3421732a16880dba8572017bbc5e3fe",
+    "revision": "28de8addc49435359073f3fc6e796537",
     "url": "/index.html"
   }
 ];
--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c1419bdfc857a78a0c9f9562620065dc.js"
+  "/precache-manifest.716b743614baab81af55c79911d2e78a.js"
 );
 
 workbox.clientsClaim();
